e90db469cea3021a1be5d3ee24d2a4fd0c3e85b6
ahmed <ahmedhashmi000@gmail.com>
2022-09-26 11:14:07 +0530

    Commited by Ahmed - 26/09 (Employee_table, Act_list)

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-   path('' , views.home, name='home')
+   path('' , views.home, name='home'),
+   path('Activity_list',views.Act_list, name='Activity_list')
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def Act_list(request):
+    return render (request, 'Act_list.html')
